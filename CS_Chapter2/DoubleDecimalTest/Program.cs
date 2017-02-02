c6ebda0c8be4fc45eb1beac9896ebdc0a09714ec
smguydish <smguydish@gmail.com>
2017-02-02 13:23:40 -0500

    Double Decimal

--- CS_Chapter2/DoubleDecimalTest/Program.cs
+++ CS_Chapter2/DoubleDecimalTest/Program.cs
@@ -9,6 +9,10 @@ namespace DoubleDecimalTest
     {
         static void Main(string[] args)
         {
+            Double X = 9999999999999999999999.0;
+            //Decimal Y = 9999999999999999999999;
+            Console.WriteLine(X);
+            //Console.WriteLine(Y);
         }
     }
 }
