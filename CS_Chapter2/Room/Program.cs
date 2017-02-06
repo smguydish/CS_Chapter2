4e4794941dd05b01f1d489ed2d9dbb41621fe063
smguydish <smguydish@gmail.com>
2017-02-06 13:53:18 -0500

    room project

--- CS_Chapter2/Room/Program.cs
+++ CS_Chapter2/Room/Program.cs
@@ -9,6 +9,10 @@ namespace Room
     {
         static void Main(string[] args)
         {
+            int length = 15;
+            int width = 25;
+            int area = (length * width);
+            Console.WriteLine("The floor space is " + area + " square feet");
         }
     }
 }
