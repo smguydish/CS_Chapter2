588568cab096d8cfeced132754255d10b2f7de03
smguydish <smguydish@gmail.com>
2017-02-13 13:25:52 -0500

    Eggs Project

--- CS_Chapter2/Eggs/Program.cs
+++ CS_Chapter2/Eggs/Program.cs
@@ -9,6 +9,14 @@ namespace Eggs
     {
         static void Main(string[] args)
         {
+            int chick1 = 13;
+            int chick2 = 59;
+            int chick3 = 45;
+            int chick4 = 10;
+            int total = (chick1 + chick2 + chick3 + chick4);
+            int dozen = (total / 12);
+            int remainder = (total % 12);
+            Console.WriteLine("A total of " + total + " eggs is " + dozen + " dozen and " + remainder + " eggs ");
         }
     }
 }
